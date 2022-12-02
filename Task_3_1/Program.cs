4a0ef7d7a343bf13e66e5a2ccf2ef7d1c42ef3e8
SaveLuciferR <cmudrec@mail.ru>
2022-12-02 20:24:08 +0300

    Добавьте файлы проекта.

---
+++ Task_3_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace Task_2_4
+{
+	class Programm
+	{
+		static double f(int n)
+		{
+			return (double)(Math.Sqrt(n) + n);
+		}
+
+		static void Main(string[] agrs)
+		{
+			double res = (f(6) / 2) + (f(13) / 2) + (f(21) / 2);
+
+			Console.WriteLine("{0:f2}", res);
+		}
+	}
+}
